2edf4613b3d2fa2dccfd7769b97fddf5e3a751a2
Ana onablo <79449819+onablo@users.noreply.github.com>
2022-01-04 16:00:37 +0200

    Added sections comments to HTML, CSS, JS files

--- js/mainHome.js
+++ js/mainHome.js
@@ -1,11 +1,27 @@
 // IMPORT
-// header
-// services
-// achievements
-// footer
+/* header */
+/* hero */
+/* clients */
+/* services */
+/* about company */
+/* hire us */
+/* achievements */
+/* promo video */
+/* testimonials */
+/* blog */
+/* contacts */
+/* footer */
 
 //FUNKCIJU PANAUDOJIMAS
-// header
-// services
-// achievements
-// footer
+/* header */
+/* hero */
+/* clients */
+/* services */
+/* about company */
+/* hire us */
+/* achievements */
+/* promo video */
+/* testimonials */
+/* blog */
+/* contacts */
+/* footer */
